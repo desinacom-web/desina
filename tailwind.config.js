34e8303c827b232d71132e46c68b1f1a38e391bf
desinacom-web <desina.com@gmail.com>
2026-04-19 22:43:18 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,6 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        // ده التعريف اللي هيخليك تستخدم كلاس font-cairo في أي مكان
         'cairo': ['Cairo', 'sans-serif'],
       },
     },
